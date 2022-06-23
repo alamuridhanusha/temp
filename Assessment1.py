325f2b68d80906afbd684f70f7bce6c921045d98
dhanu <alamuridhanusha@gmail.com>
2022-06-23 17:07:28 +0530

    success

---
+++ Assessment1.py
@@ -0,0 +1,33 @@
+#import keyword
+#print(keyword.kwlist)
+
+
+'''name="Dhanusha"
+Rollno=1206
+Percentage=85.3
+Qualification="Btech"
+Address="Gudivada"
+print('Name of student is',name)
+print(type(name))
+print('Roll number of student is',Rollno)
+print(type(Rollno))
+print('percentage of student is',Percentage)
+print(type(Percentage))
+print('Qualification of student is',Qualification)
+print(type(Qualification))
+print('Address of student is',Address)
+print(type(Address))'''
+
+'''input="HelloWorld"
+print(input)
+print(input[:5])
+print(input[5:])
+print(input[3:7])'''
+
+
+Input='LemCode'
+print(Input[:2])
+print(Input[:len(Input)-4])
+print(Input[3:9])
+print(Input[3:len(Input)])
+
